00531bcff8110cc7f610cad3d743ddf7effd2678
jrthartwig <johartwig@microsoft.com>
2024-08-15 14:50:20 -0400

    feat: Azure openai integration

--- AzureOpenAICompleteChatWith4o.cs
+++ AzureOpenAICompleteChatWith4o.cs
@@ -1,6 +1,10 @@
+using System;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
+using Azure;
+using Azure.AI.OpenAI;
+using Azure.Identity;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
@@ -9,6 +13,8 @@ using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
+using OpenAI.Chat;
+
 
 namespace Microsoft.Function
 {
@@ -29,18 +35,29 @@ namespace Microsoft.Function
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
+            string keyFromEnvironment = Environment.GetEnvironmentVariable("AZURE_OPENAI_API_KEY");
+            string endpointFromEnvironment = Environment.GetEnvironmentVariable("AZURE_OPENAI_ENDPOINT");
+            string modelFromEnvironment = Environment.GetEnvironmentVariable("AZURE_OPENAI_MODEL");
 
-            string name = req.Query["name"];
+            AzureOpenAIClient azureClient = new(
+            new Uri(endpointFromEnvironment),
+            new AzureKeyCredential(keyFromEnvironment));
+            ChatClient chatClient = azureClient.GetChatClient(modelFromEnvironment);
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            ChatCompletion completion = chatClient.CompleteChat(
+                [
+                // System messages represent instructions or other guidance about how the assistant should behave
+                new SystemChatMessage("You are a helpful assistant that talks like a pirate."),
+                // User messages represent user input, whether historical or the most recen tinput
+                new UserChatMessage("Hi, can you help me?"),
+                // Assistant messages in a request represent conversation history for responses
+                new AssistantChatMessage("Arrr! Of course, me hearty! What can I do for ye?"),
+                new UserChatMessage("What's the best way to train a parrot?"),
+            ]);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            Console.WriteLine($"{completion.Role}: {completion.Content[0].Text}");
 
-            return new OkObjectResult(responseMessage);
+            return new OkObjectResult(completion.Content[0].Text);
         }
     }
 }
