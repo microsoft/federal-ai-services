b93ec353cb1edcb44fb89c8d56cdd54235e4893b
jrthartwig <johartwig@microsoft.com>
2024-08-15 11:24:13 -0400

    feat: function app boilerplate

---
+++ AzureOpenAICompleteChatWith4o.cs
@@ -0,0 +1,47 @@
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+
+namespace Microsoft.Function
+{
+    public class AzureOpenAICompleteChatWith4o
+    {
+        private readonly ILogger<AzureOpenAICompleteChatWith4o> _logger;
+
+        public AzureOpenAICompleteChatWith4o(ILogger<AzureOpenAICompleteChatWith4o> log)
+        {
+            _logger = log;
+        }
+
+        [FunctionName("AzureOpenAICompleteChatWith4o")]
+        [OpenApiOperation(operationId: "Run", tags: new[] { "name" })]
+        [OpenApiParameter(name: "name", In = ParameterLocation.Query, Required = true, Type = typeof(string), Description = "The **Name** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
+
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+
+            return new OkObjectResult(responseMessage);
+        }
+    }
+}
+
